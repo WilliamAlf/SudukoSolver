2beeb5b70659d33a6384c8d87b98d21a4908ccba
William Alfredsson <wial2371@it.uu.se>
2023-12-02 23:50:02 +0100

    working algorithm for trivial puzzels

--- SudukoSolver.py
+++ SudukoSolver.py
@@ -89,13 +89,14 @@ def create_solver_matrix(puzzle) -> []:
 
 
 def remove_possible_value(solver_matrix, square_index, value):
-    affected_box = get_box(solver_matrix, square_index)
+    affected_box = get_box(solver_matrix, square_index)                 # Should sort out solved at this stage
     affected_row = get_row(solver_matrix, square_index)
     affected_column = get_column(solver_matrix, square_index)
-    affeced_squares = {affected_box + affected_row + affected_column} # Contains Duplicates
+    affected_squares = affected_box + affected_row + affected_column    # Contains lots of duplicates
 
-    for solver_square in affeced_squares:
-        solver_square["possible_values"] -= {value}
+    for solver_square in affected_squares:
+        if not solver_square["solved"]:
+            solver_square["possible_values"] -= {value}
 
     return solver_matrix
 
@@ -112,28 +113,12 @@ def fill_value(solver_matrix, square_index, value) -> []:
 
 
 def print_solution(solution):
-    print("- - - - - - - - -")
-    for i in range(len(solution)):
-        if i % 9 == 0:
-            print("|")
-        print(solution[i]["solution"] + " ")
+    i = 0
+    while i < len(solution):
+        print(solution[i]["solution"], end=" ")
         if (i + 1) % 9 == 0:
-            print("|")
-        if i in {26, 53}:
-            print("- - - - - - - - -")
-    print("- - - - - - - - -")
-
-"""
-0  1  2  3  4  5  6  7  8
-9  10 11 12 13 14 15 16 17
-18 19 20 21 22 23 24 25 26
-27 28 29 30 31 32 33 34 35
-36 37 38 39 40 41 42 43 44
-45 46 47 48 49 50 51 52 53
-54 55 56 57 58 59 60 61 62
-63 64 65 66 67 68 69 70 71
-72 73 74 75 76 77 78 79 80
-"""
+            print("\n")
+        i += 1
 
 
 def solve_puzzle(puzzle) -> []:
@@ -145,7 +130,7 @@ def solve_puzzle(puzzle) -> []:
 
     def filling_iteration(unsolved_squares, solver_matrix):
         remaining_unsolved_squares = unsolved_squares.copy()
-        for square_index in remaining_unsolved_squares:
+        for square_index in unsolved_squares:
             solver_square = solver_matrix[square_index]
 
             if len(solver_square["possible_values"]) == 1:
@@ -172,4 +157,24 @@ TODO :
     2. Optimise program
     3. Make program able to detect if puzzle is unsolvable
     4. Start working on computer vision part
+
+Algorithm Improvements:
+    1. Search boxes, columns and rows for possible values that only occurs once within the container
+    2. 
+
+Code Improvements :
+    1. Make squares objects
 """
+
+"""
+0  1  2  3  4  5  6  7  8
+9  10 11 12 13 14 15 16 17
+18 19 20 21 22 23 24 25 26
+27 28 29 30 31 32 33 34 35
+36 37 38 39 40 41 42 43 44
+45 46 47 48 49 50 51 52 53
+54 55 56 57 58 59 60 61 62
+63 64 65 66 67 68 69 70 71
+72 73 74 75 76 77 78 79 80
+"""
+
